6a9d800c1e42cd23a54d5e80b9ece83cc866f9b5
Mathias Arens <mathias.arens@googlemail.com>
2018-12-17 11:21:33 -0800

    LRU Cache implementation using LinkedHashMap. If no order or sort is given, LinkedHashMap keeps the order how elements were added to the LinkedHashMap. This can be used to build a LRU cache.

---
+++ src/LRUCache.java
@@ -0,0 +1,61 @@
+import java.util.*;
+/*
+https://leetcode.com/problems/lru-cache/
+ */
+
+public class LRUCache {
+    private LinkedHashMap<String, Integer> lru = new LinkedHashMap<>();
+    private int capacity;
+
+    public void set(final String key, final Integer value) {
+        if (lru.containsKey(key)) {
+            lru.remove(key);
+        } else if (lru.size() >= capacity) {
+            Iterator<String> it = lru.keySet().iterator();
+            it.next();
+            it.remove();
+        }
+        lru.put(key, value);
+    }
+
+    public int get(final String key) {
+        final Integer value = lru.get(key);
+        if (value == null) {
+            return -1;
+        } else {
+            this.set(key, value);
+            return value;
+        }
+    }
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public static void main(String args[]) {
+        // enable assert vm options with -ea
+        final LRUCache lruCache = new LRUCache(2);
+        lruCache.set("one", 1);
+        lruCache.set("two", 2);
+        assert(lruCache.get("two")==2);
+        lruCache.set("three", 3);
+        assert(lruCache.get("one")==-1);
+        assert(lruCache.get("three")==3);
+        assert(lruCache.get("two")==2);
+        lruCache.set("four", 4);
+        assert(lruCache.get("one")==-1);
+        assert(lruCache.get("three")==-1);
+        assert(lruCache.get("two")==2);
+        assert(lruCache.get("four")==4);
+        lruCache.set("five", 5);
+        assert(lruCache.get("one")==-1);
+        assert(lruCache.get("three")==-1);
+        assert(lruCache.get("two")==-1);
+        assert(lruCache.get("four")==4);
+        assert(lruCache.get("five")==5);
+    }
+}
+
+
+
+
